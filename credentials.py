11286eb39130f37678ad39ce88b6af0a58f19fa8
Juan Anacona <juanmanuelanaconamorales7@email.com>
2024-01-24 12:14:56 -0500

    First commit. The script works correctly, uses the NASA API and returns the user an image, the title, the date it was taken, and the description.

---
+++ credentials.py
@@ -0,0 +1,7 @@
+def token():
+    #This function returns the token of the Telegram bot that this script will use.
+    return "your_token"
+
+def apiKey():
+    #This function returns the API key needed to use the NASA API
+    return "your_apikey"
